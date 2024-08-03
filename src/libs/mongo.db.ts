38cbc2a808948d7fea9363a92dc6f71628bdac6f
nikhil kumar <nikhil012nk@gmail.com>
2024-08-04 02:50:45 +0530

    traillins slash added

--- src/libs/mongo.db.ts
+++ src/libs/mongo.db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const mongoConnection = async () => {
     if (mongoose.connection.readyState >= 1) return;
-    return mongoose.connect(process.env.MONGO_URI||"");
+    return mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI!);
 };
 
 export default mongoConnection;
